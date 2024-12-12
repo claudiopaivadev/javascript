24f61043b07b5335e1ae17da66768072421e578b
claudiopaivadev <DEV.clau@1712>
2024-12-12 17:11:36 -0300

    adc imagens ex014

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -1,1 +1,23 @@
-alert('Olá!')
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    
+    if (hora >= 0 && hora < 12){
+        //Bom dia!
+        img.src = 'manha.jpg'
+        document.body.style.background = '#888888'
+    }
+    else if (hora >=12 && hora <= 18){
+        //Boa tarde!
+        img.src = 'tarde.jpg'
+        document.body.style.background = '#00eeff'
+    }
+    else{
+        //Boa noite!
+        img.src = 'noite.jpg'
+        document.body.style.background = '#0a0180'
+    }
+}
