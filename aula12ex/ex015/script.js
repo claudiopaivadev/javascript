6074424a1e6609e68102af561df6d5378d13a466
claudiopaivadev <DEV.clau@1712>
2024-12-16 19:26:57 -0300

    atualização ex015

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -2,4 +2,21 @@ function verificar() {
     var data = new Date()
     var ano = data.getFullYear()
     var fano = window.document.getElementById('txtano')
+    var res = window.document.querySelector('div#res')
+    if(fano.value.length == 0 || fano.value > ano) {
+        window.alert('[ERRO] Verifique os dados e tente novamente!')
+    }
+    else{
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var genero = ''
+        if (fsex[0].checked) {
+            genero = 'Homem'
+        } 
+        else if (fsex[1].checked) {
+            genero = 'Mulher'
+        }
+        res.style.textalign = 'center'
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+    }
 }
