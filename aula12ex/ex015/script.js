708edd69b9701a58dbe13597d984abea0349393b
claudiopaivadev <DEV.clau@1712>
2024-12-16 20:02:47 -0300

    verificando sexo e idade ex015

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -10,13 +10,49 @@ function verificar() {
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
         var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+
         if (fsex[0].checked) {
             genero = 'Homem'
+            if(idade>=0 && idade < 14) {
+                //Criança
+                img.setAttribute('src', 'imagens/homemcriança.jpg')
+            }
+            else if (idade < 25) {
+                //Jovem
+                img.setAttribute('src', 'imagens/homemjovem.jpg')
+            }
+            else if (idade < 50) {
+                //Adulto
+                img.setAttribute('src', 'imagens/homemadulto.jpg')
+            }
+            else{
+                //Idoso
+                img.setAttribute('src', 'imagens/homemvelho.jpg')
+            }
         } 
         else if (fsex[1].checked) {
             genero = 'Mulher'
+            if(idade>=0 && idade < 14) {
+                //Criança
+                img.setAttribute('src', 'imagens/mulhercriança.jpg')
+            }
+            else if (idade < 25) {
+                //Jovem
+                img.setAttribute('src', 'imagens/mulherjovem.jpg')
+            }
+            else if (idade < 50) {
+                //Adulto
+                img.setAttribute('src', 'imagens/mulheradulta.jpg')
+            }
+            else{
+                //Idoso
+                img.setAttribute('src', 'imagens/mulhervelha.jpg')
+            }
         }
         res.style.textalign = 'center'
         res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img)
     }
 }
