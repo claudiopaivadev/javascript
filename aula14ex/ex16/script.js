70a7218557c6a40b84544b4c72a0bd23693e42ee
claudiopaivadev <DEV.clau@1712>
2024-12-27 12:25:31 -0300

    atualização do contador

--- aula14ex/ex16/script.js
+++ aula14ex/ex16/script.js
@@ -1,33 +1,37 @@
 function contar() {
     var n1 = document.getElementById('n1')
-    var num1 = Number(n1.value)
     var n2 = document.getElementById('n2')
-    var num2 = Number(n2.value)
     var passo = document.getElementById('passo')
-    var passo3 = Number(passo.value)
     var calculo = document.getElementById('calculo')
 
 
-    if (n1.value.length == 0 || passo.value.length == 0) {
+    if (n1.value.length == 0 || n2.value.length == 0 || passo.value.length == 0) {
         calculo.innerHTML = 'Impossivel contar!'
+        // window.alert('[ERRO] Faltam dados!')
     }
+
     else{
-        //calculo.innerHTML = `${num1} ${num2} ${passo3}`
+        calculo.innerHTML = 'Contando: <br>'
+        var num1 = Number(n1.value)
+        var num2 = Number(n2.value)
+        var passo3 = Number(passo.value)
 
-        do{
-            calculo.innerHTML(num1)
-            num1 = num1 + passo3
-        } while (num1 <= num2)
+        if(passo3 <= 0){ 
+            window.alert('Passo inválido! considerando PASSO 1')
+            passo3 = 1
+        }
 
-        /*var x = num1
-        while (x <= num2) {
-            calculo.innerHTML(x)
-            x = x + passo3
-        }*/
-
-        /*for (num1; num1 <= num2; num1 = num1 + passo3) {
-            calculo.innerHTML == `${num1}`
-        }*/
+        if(num1 < passo3){
+            for (x = num1; x <= num2; x += passo3) {
+            calculo.innerHTML += `${x} \u{1F449}`
+            } 
+            calculo.innerHTML +=`\u{1F3C1}`
+        }
+        else{
+            for(x = num1; x >= num2; x-=passo3) {
+                calculo.innerHTML += `${x} \u{1F449}`
+            }
+            calculo.innerHTML +=`\u{1F3C1}`
+        }
     }
-
 }
